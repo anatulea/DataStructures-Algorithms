659951c6113001dacf3f2077e57f9361ebd75425
Ana Tulea <anatulea14@gmail.com>
2020-09-21 22:14:49 -0700

    added priority queues using heap

--- src/Trees/binarySearchTrees.py
+++ src/Trees/binarySearchTrees.py
@@ -1,12 +1,12 @@
-#Binary Search Trees are a non-linear data structure.
-#They consist of a root node and zero, one or two children where the children can again have 0,1, or 2 nodes as their children and so on
-#In most cases, the time complexity of operations on a BST, which include, lookups, insertions and deletions, take O(log N) time
-#Except for the worst case, where the tree is heavily unbalanced with all the nodes being on one side of the tree.
-#In that case, it basically becomes a linked list and the time complexities go up to O(n)
+# Binary Search Trees are a non-linear data structure.
+# They consist of a root node and zero, one or two children where the children can again have 0,1, or 2 nodes as their children and so on
+# In most cases, the time complexity of operations on a BST, which include, lookups, insertions and deletions, take O(log N) time
+# Except for the worst case, where the tree is heavily unbalanced with all the nodes being on one side of the tree.
+# In that case, it basically becomes a linked list and the time complexities go up to O(n)
 
-#Lets implement an unbalanced Binary Search Tree first
-#We will need a node class to store information about each node
-#It will store the data and the pointers to its left and right children
+# Lets implement an unbalanced Binary Search Tree first
+# We will need a node class to store information about each node
+# It will store the data and the pointers to its left and right children
 class Node():
     def __init__(self, data):
         self.data = data
@@ -14,21 +14,22 @@ class Node():
         self.right = None
 
 
-#Now we will implement the Binary Search Tree having a constructor with the root node initialised to None
-#And the three methods, lookup, insert and delete
+# Now we will implement the Binary Search Tree having a constructor with the root node initialised to None
+# And the three methods, lookup, insert and delete
 class BST():
     def __init__(self):
         self.root = None
         self.number_of_nodes = 0
 
 
-#For the insert method, we check if the root node is None, then we make the root node point to the new node
-#Otherwise, we create a temporary pointer which points to the root node at first.
-#Then we compare the data of the new node to the data of the node pointed by the temporary node.
-#If it is greater then first we check if the right child of the temporary node exists, if it does, then we update the temporary node to its right child
-#Otherwise we make the new node the right child of the temporary node
-#And if the new node data is less than the temporary node data, we follow the same procedure as above this time with the left child.
-#The complexity is O(log N) in avg case and O(n) in worst case.
+# For the insert method, we check if the root node is None, then we make the root node point to the new node
+# Otherwise, we create a temporary pointer which points to the root node at first.
+# Then we compare the data of the new node to the data of the node pointed by the temporary node.
+# If it is greater then first we check if the right child of the temporary node exists, if it does, then we update the temporary node to its right child
+# Otherwise we make the new node the right child of the temporary node
+# And if the new node data is less than the temporary node data, we follow the same procedure as above this time with the left child.
+# The complexity is O(log N) in avg case and O(n) in worst case.
+
     def insert(self, data):
         new_node = Node(data)
         if self.root == None:
@@ -52,10 +53,12 @@ class BST():
             return
 
 
-#Now we will implement the lookup method.
-#It will follow similar logic as to the insert method to reach the correct position.
-#Only instead of inserting a new node we will return "Found" if the node pointed by the temporary node contains the same value we are looking for
-    def search(self,data):
+# Now we will implement the lookup method.
+# It will follow similar logic as to the insert method to reach the correct position.
+# Only instead of inserting a new node we will return "Found" if the node pointed by the temporary node contains the same value we are looking for
+
+
+    def search(self, data):
         if self.root == None:
             return "Tree Is Empty"
         else:
@@ -71,23 +74,24 @@ class BST():
                     current_node = current_node.right
 
 
-#Finally comes the very complicated remove method.
-#This one is too complicated for me to explain while writing. So I'll just write the code down with some comments
-#explaining which conditions are being checked
+# Finally comes the very complicated remove method.
+# This one is too complicated for me to explain while writing. So I'll just write the code down with some comments
+# explaining which conditions are being checked
+
     def remove(self, data):
-        if self.root == None: #Tree is empty
+        if self.root == None:  # Tree is empty
             return "Tree Is Empty"
         current_node = self.root
         parent_node = None
-        while current_node!=None: #Traversing the tree to reach the desired node or the end of the tree
+        while current_node != None:  # Traversing the tree to reach the desired node or the end of the tree
             if current_node.data > data:
                 parent_node = current_node
                 current_node = current_node.left
             elif current_node.data < data:
                 parent_node = current_node
                 current_node = current_node.right
-            else: #Match is found. Different cases to be checked
-                #Node has left child only
+            else:  # Match is found. Different cases to be checked
+                # Node has left child only
                 if current_node.right == None:
                     if parent_node == None:
                         self.root = current_node.left
@@ -100,7 +104,7 @@ class BST():
                             parent_node.right = current_node.left
                             return
 
-                #Node has right child only
+                # Node has right child only
                 elif current_node.left == None:
                     if parent_node == None:
                         self.root = current_node.right
@@ -113,9 +117,9 @@ class BST():
                             parent_node.right = current_node.right
                             return
 
-                #Node has neither left nor right child
+                # Node has neither left nor right child
                 elif current_node.left == None and current_node.right == None:
-                    if parent_node == None: #Node to be deleted is root
+                    if parent_node == None:  # Node to be deleted is root
                         current_node = None
                         return
                     if parent_node.data > current_node.data:
@@ -125,26 +129,24 @@ class BST():
                         parent_node.right = None
                         return
 
-                #Node has both left and right child
+                # Node has both left and right child
                 elif current_node.left != None and current_node.right != None:
                     del_node = current_node.right
                     del_node_parent = current_node.right
-                    while del_node.left != None: #Loop to reach the leftmost node of the right subtree of the current node
+                    while del_node.left != None:  # Loop to reach the leftmost node of the right subtree of the current node
                         del_node_parent = del_node
                         del_node = del_node.left
-                    current_node.data = del_node.data #The value to be replaced is copied
-                    if del_node == del_node_parent: #If the node to be deleted is the exact right child of the current node
+                    current_node.data = del_node.data  # The value to be replaced is copied
+                    if del_node == del_node_parent:  # If the node to be deleted is the exact right child of the current node
                         current_node.right = del_node.right
                         return
-                    if del_node.right == None: #If the leftmost node of the right subtree of the current node has no right subtree
+                    if del_node.right == None:  # If the leftmost node of the right subtree of the current node has no right subtree
                         del_node_parent.left = None
                         return
-                    else: #If it has a right subtree, we simply link it to the parent of the del_node
+                    else:  # If it has a right subtree, we simply link it to the parent of the del_node
                         del_node_parent.left = del_node.right
                         return
         return "Not Found"
-
-
 
 
 my_bst = BST()
@@ -218,4 +220,4 @@ my_bst.insert(10)
         10
 '''
 print(my_bst.root.data)
-#10
+# 10
